11789193ffdfa77f5e8929431e362221b7595a16
magomarques <auricleciomp@hotmail.com>
2021-09-27 15:50:57 -0300

    Alura - Curso NodeJS com Express - Aula 04

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -8,21 +8,47 @@ class Atendimento {
 
         //const dataCriacao = new Date()
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-
-        const sql = 'INSERT INTO Atendimentos SET ?'
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if (erro) {
-                //console.log(erro)
-                res.status(400).json(erro)
-            } else {
-                //console.log(resultados)
-                res.status(201).json(resultados)
+        
+        // Validações:
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteValido = atendimento.cliente.length >= 5
+        
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteValido,
+                mensagem: 'Cliente deve ter no mínimo 5 caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    //console.log(erro)
+                    res.status(400).json(erro)
+                } else {
+                    //console.log(resultados)
+                    res.status(201).json(resultados)
+                }
+            })
+
+        }
+ 
     }
 
 }
